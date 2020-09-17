6f2f904c4da9a2d714fd95380ef73159f52e76a1
mkopsho <michael.kopsho@gmail.com>
2020-09-17 16:17:25 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
-
   render() {
-    return(
+    return (
       <div>
-        Band Component
+        Band
+        <li>
+          <span>{this.props.band.name}</span>
+          <button onClick={() => this.props.deleteBand(this.props.band.id)}>
+            Delete
+        </button>
+        </li>
       </div>
     );
   }
 };
 
-export default Band;
+export default Band
